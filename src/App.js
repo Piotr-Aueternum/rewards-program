89a796e2bbc68726a49e8c460387f7343b9709c0
Piotr Sochacz <piotr.aueternum@gmail.com>
2023-09-05 16:35:12 +0200

    Added rewards program component to main view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { RewardsProgram } from "./modules";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="App-container">
+        <RewardsProgram />
+      </div>
     </div>
   );
 }
