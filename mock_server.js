57963b9cb576cb60ddbe473766793cc05f9c98d4
Piotr Sochacz <piotr.aueternum@gmail.com>
2023-09-05 14:46:33 +0200

    Added mock data to endpoint

--- mock_server.js
+++ mock_server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3010
+const express = require("express");
+const customers_extended = require("./mocks/customers_extended.json");
+const app = express();
+const port = 3010;
 
-app.get('/api/customers/rewards', (req, res) => {
-    res.json([])
-})
+app.get("/api/customers/rewards", (req, res) => {
+  res.json(customers_extended);
+});
 
 app.listen(port, () => {
-    console.log(`Mock server listen on port ${port}`)
-})
+  console.log(`Mock server listen on port ${port}`);
+});
