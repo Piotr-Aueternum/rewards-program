25dbb6f076ab53bf914064a1912410a12cf506c4
Piotr Sochacz <piotr.aueternum@gmail.com>
2023-09-05 16:01:10 +0200

    Added format transactions to desired shape

--- src/modules/RewardsProgram/services/useFetchRewards.js
+++ src/modules/RewardsProgram/services/useFetchRewards.js
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import api from "services/api";
+import sumTransactionsPoints from "../utils/sumTransactionsPoints";
+import getTransactionsMonthsCount from "../utils/getTransactionsMonthsCount";
 
 function useFetchRewards() {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  function formatTransactions(transactions) {
+    return transactions.map((row) => {
+      const pointsSum = sumTransactionsPoints(row.transactions);
+      const pointsPerMonth = Math.floor(
+        pointsSum / getTransactionsMonthsCount(row.transactions),
+      );
+      return {
+        key: row.id,
+        customer: row.fullName,
+        perMonth: pointsPerMonth,
+        total: pointsSum,
+      };
+    });
+  }
   async function fetchRewards() {
     try {
       setLoading(true);
       const response = await api.get("/customers/rewards");
-      setResult(response);
+      const results = formatTransactions(response);
+      setResult(results);
       setLoading(false);
     } catch (error) {
       setLoading(false);
